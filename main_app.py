b9309447cbfdca9c9c221d05b1ffe81182b359f0
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-11-14 19:52:42 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,2 +1,57 @@
 # main app codes
-print ("Welcome")
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# For static images:
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+    image = cv2.flip(cv2.imread('path/to/your/image.jpg'), 1)
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # Print the hand landmarks
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            print(hand_landmarks)
+
+    cv2.imshow('MediaPipe Hands', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # Flip the image horizontally for a selfie-view display.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_mp_drawing_styles.get_default_hand_connection_style())
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+cap.release()
+cv2.destroyAllWindows()
