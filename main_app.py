89f143deb820dc3dc593e344867909195738fb53
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-11-14 21:54:11 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,43 +1,28 @@
-# main app codes
 import cv2
 import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
+# Initialize MediaPipe's hands module
 mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
 
-# For static images:
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-    image = cv2.flip(cv2.imread('gesture_images/up.jpg'), 1)
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Print the hand landmarks
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            print(hand_landmarks)
-
-    cv2.imshow('MediaPipe Hands', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-# For webcam input:
+# For webcam input
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
             print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
+            # If loading a video, you might want to break the loop here.
             continue
 
         # Flip the image horizontally for a selfie-view display.
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1)
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image)
 
         # Draw the hand annotations on the image.
@@ -49,9 +34,13 @@ with mp_hands.Hands(
                     image,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_mp_drawing_styles.get_default_hand_connection_style())
+                    mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                )
+
         cv2.imshow('MediaPipe Hands', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
+
 cap.release()
 cv2.destroyAllWindows()
