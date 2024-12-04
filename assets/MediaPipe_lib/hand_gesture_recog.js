00cabbd645cab97df0ee9d0a6bc692f187e0e60e
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-04 19:46:55 +0300

    Update hand_gesture_recog.js

--- assets/MediaPipe_lib/hand_gesture_recog.js
+++ assets/MediaPipe_lib/hand_gesture_recog.js
@@ -1,8 +1,1 @@
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    const video = document.getElementById('video');
-    video.srcObject = stream;
-  })
-  .catch(err => {
-    console.error('Error accessing 1  webcam:', err);
-  });
+
