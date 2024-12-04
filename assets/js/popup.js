cefa4579bbbe793a11391e02177034946d0ac8da
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-04 11:57:27 +0300

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -8,17 +8,17 @@ var span = document.getElementsByClassName("closex")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modalx.style.display = "block";
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modalx.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modalx) {
-    modalx.style.display = "none";
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
