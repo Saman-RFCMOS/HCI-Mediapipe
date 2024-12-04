6cf00db4bf5b29b5014718af533fd834ebb6ea77
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-04 19:47:59 +0300

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -22,3 +22,11 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    const video = document.getElementById('video');
+    video.srcObject = stream;
+  })
+  .catch(err => {
+    console.error('Error accessing 1  webcam:', err);
+  });
