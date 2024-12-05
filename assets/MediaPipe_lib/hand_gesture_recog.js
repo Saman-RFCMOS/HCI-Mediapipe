c92bd64f803b545ffe35a8982f82310397f7baba
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-05 20:36:01 +0300

    Update hand_gesture_recog.js

--- assets/MediaPipe_lib/hand_gesture_recog.js
+++ assets/MediaPipe_lib/hand_gesture_recog.js
@@ -1,7 +1,7 @@
 async function initializeGestureRecognizer() {
   // No need to import FilesetResolver - it's available globally
   const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm/"
   );
   
   const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
