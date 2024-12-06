50ecd892604f5c765a5f59c6f52b91ff61a78199
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-06 22:57:32 +0300

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,16 +1,7 @@
-var modal = document.getElementById("survey_popup");
-var btn = document.getElementById("open_survey");
-var span = document.getElementsByClassName("closex")[0];
-btn.onclick = function () {
-  modal.style.display = "block";
-}
 
-span.onclick = function () {
-  modal.style.display = "none";
+function showPopup() {
+  document.getElementById('popupOverlay').style.display = 'flex';
 }
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function hidePopup() {
+  document.getElementById('popupOverlay').style.display = 'none';
 }
