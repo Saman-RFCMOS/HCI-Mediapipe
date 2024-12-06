b5d3d000292bd368e36ee0f7f9d0fa872f9f01b2
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-06 23:22:49 +0300

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,8 +4,6 @@ var span = document.getElementsByClassName("closex")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
