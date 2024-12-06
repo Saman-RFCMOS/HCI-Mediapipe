5ae4954b4b5b23d22f0ade1400de6cdfb7cdc60a
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-06 22:30:04 +0300

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,23 +1,15 @@
 var modal = document.getElementById("survey_popup");
-
-// Get the button that opens the modal
 var btn = document.getElementById("open_survey");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closex")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
