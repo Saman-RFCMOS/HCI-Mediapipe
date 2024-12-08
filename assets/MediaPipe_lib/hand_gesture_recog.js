ffab9b1d4c857a21bb4bf97b275ab2ba17d4b404
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-08 14:42:22 +0300

    Update hand_gesture_recog.js

--- assets/MediaPipe_lib/hand_gesture_recog.js
+++ assets/MediaPipe_lib/hand_gesture_recog.js
@@ -1,140 +1,131 @@
 import { GestureRecognizer, FilesetResolver, DrawingUtils } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
 
-        let gestureRecognizer;
-        let runningMode = "IMAGE";
-        let webcamRunning = false;
+let gestureRecognizer;
+let runningMode = "IMAGE";
+let webcamRunning = false;
 
+const createGestureRecognizer = async () => {
+    const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+    );
+    gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath:
+                "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            delegate: "GPU"
+        },
+        runningMode: runningMode
+    });
+};
 
-        const createGestureRecognizer = async () => {
-            const vision = await FilesetResolver.forVisionTasks(
-                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-            );
-            gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
-                baseOptions: {
-                    modelAssetPath:
-                        "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
-                    delegate: "GPU"
-                },
-                runningMode: runningMode
-            });
-        };
-        const hands = new mpHands.Hands({
-          maxNumHands: 1, 
-          minDetectionConfidence: 0.5,
-          minTrackingConfidence: 0.5
+createGestureRecognizer();
+
+const videoElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const gestureOutput = document.getElementById("gesture_output");
+const enableWebcamButton = document.getElementById("open_survey");
+
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+    enableWebcamButton.addEventListener("click", enableCam);
+} else {
+    console.warn("getUserMedia() is not supported by your browser");
+}
+
+function enableCam() {
+    if (webcamRunning === true) {
+        webcamRunning = false;
+        enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+    } else {
+        webcamRunning = true;
+        enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+    }
+
+    const constraints = { video: true };
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        videoElement.srcObject = stream;
+        videoElement.addEventListener("loadeddata", predictWebcam);
+    });
+}
+
+let lastVideoTime = -1;
+let results;
+
+async function predictWebcam() {
+    const webcamElement = document.getElementById("webcam");
+
+    if (runningMode === "IMAGE") {
+        runningMode = "VIDEO";
+        await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
+    }
+
+    let nowInMs = Date.now();
+    if (videoElement.currentTime !== lastVideoTime) {
+        lastVideoTime = videoElement.currentTime;
+        results = gestureRecognizer.recognizeForVideo(videoElement, nowInMs);
+    }
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    canvasElement.style.height = "360px";
+    webcamElement.style.height = "360px";
+    canvasElement.style.width = "480px";
+    webcamElement.style.width = "480px";
+
+    const drawingUtils = new DrawingUtils(canvasCtx);
+
+    if (results.landmarks && results.landmarks.length > 0) {
+        const landmarks = results.landmarks[0];
+        drawingUtils.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, {
+            color: "#00F00F",
+            lineWidth: 3
         });
+        drawingUtils.drawLandmarks(landmarks, { color: "#FF0000", lineWidth: 1 });
+    }
 
-        createGestureRecognizer();
+    if (results.gestures.length > 0) {
+        gestureOutput.style.display = "block";
+        const categoryName = results.gestures[0][0].categoryName;
+        const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
+        const handedness = results.handednesses[0][0].displayName;
 
-        const videoElement = document.getElementById("webcam");
-        const canvasElement = document.getElementById("output_canvas");
-        const canvasCtx = canvasElement.getContext("2d");
-        const gestureOutput = document.getElementById("gesture_output");
-        const enableWebcamButton = document.getElementById("open_survey");
-
-        function hasGetUserMedia() {
-            return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+        let action;
+        switch (categoryName) {
+            case "Pointing_Up":
+                action = 1;
+                break;
+            case "Victory":
+                action = 2;
+                break;
+            case "Thumb_Up":
+                action = "Like";
+                break;
+            case "Thumb_Down":
+                action = "Dislike";
+                break;
+            case "Closed_Fist":
+                action = "Close";
+                break;
+            case "Open_Palm":
+                action = "Submit";
+                break;
+            default:
+                action = "Unknown gesture";
         }
 
-        if (hasGetUserMedia()) {
-            enableWebcamButton.addEventListener("click", enableCam);
-        } else {
-            console.warn("getUserMedia() is not supported by your browser");
-        }
+        gestureOutput.innerText = `Action: ${action}\n Confidence: ${categoryScore}%\n Handedness: ${handedness}`;
+    } else {
+        gestureOutput.style.display = "none";
+    }
 
-        function enableCam() {
-            if (webcamRunning === true) {
-                webcamRunning = false;
-                enableWebcamButton.innerText = "ENABLE PREDICTIONS";
-            } else {
-                webcamRunning = true;
-                enableWebcamButton.innerText = "DISABLE PREDICTIONS";
-            }
+    canvasCtx.restore();
 
-            const constraints = { video: true };
-            navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-                videoElement.srcObject = stream;
-                videoElement.addEventListener("loadeddata", predictWebcam);
-            });
-        }
-
-        let lastVideoTime = -1;
-        let results;
-
-        async function predictWebcam() {
-            const webcamElement = document.getElementById("webcam");
-
-            if (runningMode === "IMAGE") {
-                runningMode = "VIDEO";
-                await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
-            }
-
-            let nowInMs = Date.now();
-            if (videoElement.currentTime !== lastVideoTime) {
-                lastVideoTime = videoElement.currentTime;
-                results = gestureRecognizer.recognizeForVideo(videoElement, nowInMs);
-            }
-
-            canvasCtx.save();
-            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-            canvasElement.style.height = "360px";
-            webcamElement.style.height = "360px";
-            canvasElement.style.width = "480px";
-            webcamElement.style.width = "480px";
-
-            const drawingUtils = new DrawingUtils(canvasCtx);
-
-            if (results.landmarks) {
-                for (const landmarks of results.landmarks) {
-                    drawingUtils.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, {
-                        color: "#00F00F",
-                        lineWidth: 3
-                    });
-                    drawingUtils.drawLandmarks(landmarks, { color: "#FF0000", lineWidth: 1 });
-                }
-            }
-
-            if (results.gestures.length > 0) {
-                gestureOutput.style.display = "block";
-                const categoryName = results.gestures[0][0].categoryName;
-                const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
-                const handedness = results.handednesses[0][0].displayName;
-
-                let action;
-                switch (categoryName) {
-                    case "Pointing_Up":
-                        action = 1;
-                        break;
-                    case "Victory":
-                        action = 2;
-                        break;
-                    case "Thumb_Up":
-                        action = "Like";
-                        break;
-                    case "Thumb_Down":
-                        action = "Dislike";
-                        break;
-                    case "Closed_Fist":
-                        action = "Close";
-                        break;
-                    case "Open_Palm":
-                        action = "Submit";
-                        break;
-                    default:
-                        action = "Unknown gesture";
-                }
-
-
-                gestureOutput.innerText = `Action: ${action}\n Confidence: ${categoryScore}%\n Handedness: ${handedness}`;
-
-            } else {
-                gestureOutput.style.display = "none";
-            }
-
-            canvasCtx.restore();
-
-            if (webcamRunning === true) {
-                window.requestAnimationFrame(predictWebcam);
-            }
-        }
+    if (webcamRunning === true) {
+        window.requestAnimationFrame(predictWebcam);
+    }
+}
