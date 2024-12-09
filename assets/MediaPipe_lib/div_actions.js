5629bbc06eb480eb97f9ef3bf215d8f0983b3c7b
Saman Mokhtabadamri <saman.mokhtabadamri@sabanciuniv.edu>
2024-12-09 18:46:03 +0300

    Update div_actions.js

--- assets/MediaPipe_lib/div_actions.js
+++ assets/MediaPipe_lib/div_actions.js
@@ -1,34 +1,34 @@
-
 function getGestureFromDiv() {
     const gestureOutputDiv = document.getElementById('gesture_output');
     const gestureOutputText = gestureOutputDiv.innerText; 
     const actionMatch = gestureOutputText.match(/Action:\s*(\w+)/);
-    return actionMatch ? actionMatch[1] : null;
+    return actionMatch ? actionMatch[1] : null; 
 }
 
 function handleGesture(gesture) {
     switch (gesture) {
         case 'dislike':
-            console.log("Performing Dislike action");
+            showDiv('main_gesture'); // Show Main Gesture div
             break;
         case 'star':
-            console.log("Performing Star action");
+            showDiv('star_gesture'); // Show Star Gesture div
             break;
         case 'voice':
-            console.log("Performing Voice action");
+            showDiv('voice_gesture'); // Show Voice Feedback div
             break;
         case 'thank_you':
-            console.log("Performing Thank You action");
+            showDiv('thank_you'); // Show Thank You div
             break;
         default:
             console.log("Unknown gesture");
     }
 }
 
+
 const extractedGesture = getGestureFromDiv();
 if (extractedGesture) {
     console.log(`Extracted Gesture: ${extractedGesture}`);
-    handleGesture(extractedGesture);
+    handleGesture(extractedGesture); 
 } else {
     console.log("No gesture detected.");
 }
